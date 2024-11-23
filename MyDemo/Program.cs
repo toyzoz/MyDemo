0c2a8301afab351d5350590847ba2496d6c4bc57
toyzoz <43981303+toyzoz@users.noreply.github.com>
2024-11-23 17:09:32 +0800

    Update Program.cs

--- MyDemo/Program.cs
+++ MyDemo/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.WebHost.UseUrls("http://*:50002");
+builder.WebHost.UseUrls("http://*:50003");
 
 
 var app = builder.Build();
